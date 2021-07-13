12fd549a63797e2b50b148cc97250ae98cf965c2
EsatYasar <esatyasar91@gmail.com>
2021-07-13 03:12:34 +0300

    added restart property

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 
+function play(){
+
 let btn = document.getElementById("btn");
 let output = document.getElementById("outputtext");
 
 let number = Math.floor(Math.random()*100)+1;
 
 let count = 0;
-let guessed_nums = [];
 
 btn.addEventListener("click", function(){
     let input = document.getElementById("userInput").value;
+    input = parseInt(input);
     if (input < 1 || input > 100){
-        alert("Please enter a number between 1 -100.")
+        alert("Please enter a number between 1 - 100.")
+    }else if (Number.isInteger(input) === false){
+        alert("Please enter a Integer value")
     }else{
         if (input == number){
             output.innerHTML = "CONGRATULATIONS"
             document.getElementById("icons").setAttribute("src", "./assests/giphy.gif");
             const text = document.getElementById("title")
             text.innerHTML = `Number of guesses ${count}`
+            const button = document.getElementById("btn");
+            button.innerHTML = "Restart";
+            button.addEventListener("click", () => {
+                count = 0;
+                const text = document.getElementById("title")
+                text.innerHTML = "(1-100)"
+                document.getElementById("icons").setAttribute("src","./assests/guess-the-number.png")
+                button.innerHTML = "check"
+                play();
+            })
             }else if (input < number){
             output.innerHTML = "You guessed too low"
             document.getElementById("icons").setAttribute("src", "./assests/up.png");
@@ -32,11 +46,12 @@ btn.addEventListener("click", function(){
             const text = document.getElementById("title")
             text.innerHTML = `Number of guesses ${count}`
         }
-    }    
+    }
+    
 });
 
-
-
+}
+play();
 
 
 
